e92515cd131ee032c5df841a37822c793882cc90
Félix VIART <viart.felix2017@gmail.com>
2024-12-02 21:59:56 +0100

    #8 #13 finished add participant view + form (needs advanced validation)

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -3,32 +3,41 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\Participant;
+use App\Form\AddParticipantToEventType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route("/participants", name: "participants.")]
 class ParticipantController extends AbstractController
 {
     private EventRepository $eventRepository;
+	private EntityManagerInterface $entityManager;
 
     public function __construct(
-        EventRepository $eventRepository
+        EventRepository $eventRepository,
+		EntityManagerInterface $entityManager
     )
     {
         $this->eventRepository = $eventRepository;
+		$this->entityManager = $entityManager;
     }
 
-    /**
-     * Add a participant to an event.
-     * @param int|Event $id
-     * @return JsonResponse
-     */
+	/**
+	 * Add a participant to an event.
+	 * @param int|Event $id
+	 * @param Request $request
+	 * @return JsonResponse
+	 */
     #[Route('/add/{id}', name: 'add', requirements: [
         "id" => '\d+'
     ])]
-    public function addParticipant(int|Event $id): JsonResponse
+    public function addParticipant(int|Event $id, Request $request): Response
     {
         $event = is_int($id)
             ? $this->eventRepository->find($id)
@@ -40,10 +49,24 @@ class ParticipantController extends AbstractController
             );
         }
 
-        //TODO: add form and handle adding participant
+		$participant = new Participant();
+		$participant->setEvent($event);
 
-        return $this->json([
-            "message" => "Created successfully !"
-        ], 201);
+		$form = $this->createForm(AddParticipantToEventType::class, $participant);
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() && $form->isValid()) {
+			/** @var Participant $formParticipant */
+			$formParticipant = $form->getData();
+
+			$this->entityManager->persist($formParticipant);
+			$this->entityManager->flush();
+
+			return $this->redirectToRoute("events.single", ["id" => $event->getId()]);
+		}
+
+        return $this->render("participant/new.html.twig", [
+			"form" => $form
+		]);
     }
 }
---
+++ src/Form/AddParticipantToEventType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddParticipantToEventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('event', EntityType::class, [
+                'class' => Event::class,
+                'choice_label' => 'name',
+            ])
+			->add("save", SubmitType::class, [
+				"label" => "OK"
+			])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
